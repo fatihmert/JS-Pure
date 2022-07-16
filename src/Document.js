075aea03c60af165025a6006e4352609e6bb3fbf
Fatih Mert Doğancan <fatihmertdogancan@hotmail.com>
2022-07-16 12:42:58 +0300

    update reference

--- src/Document.js
+++ src/Document.js
@@ -1,3 +1,7 @@
+/* 
+    @ref: https://youmightnotneedjquery.com/#ready
+*/
+
 Document.prototype.ready = function(callback=()=>{}) {
     if (document.readyState != 'loading'){
         callback();
